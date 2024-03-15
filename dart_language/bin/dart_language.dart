cfab0f3974d88bee576af991660240eb434e11fc
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2024-03-15 19:00:16 +0900

    update

--- dart_language/bin/dart_language.dart
+++ dart_language/bin/dart_language.dart
@@ -1,5 +1,18 @@
-import 'package:dart_language/dart_language.dart' as dart_language;
+//
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_language.calculate()}!');
+void main() {
+  List<int> _numbers = List.generate(10, (i) => i);
+  print(_numbers);
+  _numbers = _numbers..map((e) => e == 3);
+  // _numbers.removeWhere((e) => e % 2 == 0);
+  // _numbers.map((e) => e + e).toList();
+
+  // for (int i = 0; i < _numbers.length; i++) {
+  //   if (_numbers[i] % 2 == 0) {
+  //     _numbers.removeAt(i);
+  //   } else {
+  //     _numbers[i] = _numbers[i] * 2;
+  //   }
+  // }
+  print(_numbers);
 }
