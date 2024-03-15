3d808087678d5cdca0cbb5e09afc7b8a6c4c758b
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2024-03-16 01:29:25 +0900

    update

--- dart_language/bin/dart_language.dart
+++ dart_language/bin/dart_language.dart
@@ -2,17 +2,8 @@
 
 void main() {
   List<int> _numbers = List.generate(10, (i) => i);
-  print(_numbers);
-  _numbers = _numbers..map((e) => e == 3);
-  // _numbers.removeWhere((e) => e % 2 == 0);
-  // _numbers.map((e) => e + e).toList();
-
-  // for (int i = 0; i < _numbers.length; i++) {
-  //   if (_numbers[i] % 2 == 0) {
-  //     _numbers.removeAt(i);
-  //   } else {
-  //     _numbers[i] = _numbers[i] * 2;
-  //   }
-  // }
-  print(_numbers);
+  for (int i = _numbers.length - 1; -1 < i; i--) {
+    print(i);
+    print(_numbers[i]);
+  }
 }
