0e012aefc4585dd3a1c08d5413a369f6da6ca5d2
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2024-03-19 01:52:06 +0900

    update

--- dart_language/bin/dart_language.dart
+++ dart_language/bin/dart_language.dart
@@ -1,27 +1,55 @@
 //
 
 void main() {
-  final G70 g70 = G70("G70");
-  final G80 g80 = G80("G80");
-  g70.printName();
-  g80.printName();
+  // G70 g70 = G70(subName: "Sedan");
+  // g70.getHorsePower();
+  Tyger tyger = Tyger();
+  tyger.log();
 }
 
 class Car {
+  final int hp;
   final String name;
 
-  const Car(this.name);
+  const Car({this.name = "", this.hp = 0});
 
-  void printName() => print("자동차 이름은 $name 입니다");
+  void getHorsePower() => print("$name 마력 : $hp");
 }
 
 class G80 extends Car {
-  const G80(super.name);
+  const G80({super.name = "G80", super.hp = 250});
 }
 
 class G70 extends Car {
-  const G70(super.name);
-
-  // @override
-  void printName() => print("$name 입니다");
+  final String subName;
+  const G70({
+    super.name = "G70",
+    super.hp = 230,
+    required this.subName,
+  });
 }
+
+class Person {
+  final String name = "Empty";
+}
+
+class Tyger extends Person {
+  final String name = "Tyger";
+
+  void log() => print("Person name: ${super.name}, Tyger name: ${this.name}");
+}
+
+// class ParentClass {
+//   String shot = "This is a shot on Super Keyword";
+// }
+
+// // Creating child class
+// class SubClass extends ParentClass {
+//   final String shot = "Educative";
+//   // Accessing parent class variable
+//   void showMessage() {
+//     print(super.shot);
+
+//     print("$shot has ${shot.length} letters.");
+//   }
+// }
