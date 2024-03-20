7f7a4a8b067ab52c329287d6ba102c961472b640
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2024-03-20 23:55:31 +0900

    update

--- dart_language/bin/dart_language.dart
+++ dart_language/bin/dart_language.dart
@@ -1,16 +1,20 @@
 //
 
-void main() {}
-
-class Test {
-  void log() => print("Test !!");
+void main() {
+  G70 g70 = G70("G70");
+  g70.getHorsePower(100);
+  print(g70.name);
 }
 
-class TestImplements implements Test {
+class Car {
+  final String name;
+  const Car(this.name);
+  void getHorsePower(int hp) => print("마력은 $hp 입니다.");
+}
+
+class G70 implements Car {
   @override
-  void log() {
-    // TODO: implement log
-  }
+  String get name => throw UnimplementedError();
+  @override
+  void getHorsePower(int hp) {}
 }
-
-class TestExtends extends Test {}
