04b4fb448676b88e845dbe65992a898a0e5e97ba
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2024-03-22 14:54:22 +0900

    update

--- dart_language/bin/dart_language.dart
+++ dart_language/bin/dart_language.dart
@@ -1,18 +1,82 @@
 //
 
-void main() {}
-
-class Hero {
-  final int tier;
-  const Hero({required this.tier});
-
-  int getPower() => (10 - tier) * 100;
+void main() {
+  C c = C();
+  c.log();
 }
 
-class IronMan extends Hero {
-  const IronMan({super.tier = 9});
+// class AClass
+
+mixin A {
+  void log() => print("A mixin");
+}
+mixin B {
+  void log() => print("B mixin");
+}
+mixin D {
+  void log() => print("D mixin");
 }
 
-class Hulk extends Hero {
-  const Hulk({super.tier = 7});
-}
+class C with A, B, D {}
+
+
+// void main() {
+//   IronMan ironMan = IronMan();
+//   SuperMan superMan = SuperMan();
+//   print(
+//       "${ironMan.name}의 공격력은 ${ironMan.getAttack()}, 방어력은 ${ironMan.getDefence()}이다.");
+//   print(
+//       "${superMan.name}의 공격력은 ${superMan.getAttack()}, 방어력은 ${superMan.getDefence()}이다.");
+// }
+
+// abstract class Hero {
+//   final String name;
+//   const Hero(this.name);
+
+//   int getAttack();
+//   int getDefence();
+// }
+
+// class IronMan implements Hero {
+//   final int tier = 9;
+//   @override
+//   int getAttack() => (10 - tier) * 100;
+
+//   @override
+//   int getDefence() => (10 - tier) * 20;
+
+//   @override
+//   String get name => "IronMan";
+// }
+
+// class Hulk implements Hero {
+//   final int tier = 7;
+//   @override
+//   int getAttack() => (10 - tier) * 100;
+
+//   @override
+//   int getDefence() => (10 - tier) * 20;
+
+//   @override
+//   String get name => "Hulk";
+// }
+
+// class SuperMan implements Hero {
+//   @override
+//   int getAttack() => 1000;
+
+//   @override
+//   int getDefence() => 100;
+
+//   @override
+//   String get name => "SuperMan";
+// }
+
+// // class Hulk extends Hero {
+// //   final String name = "Hulk";
+// //   const Hulk({super.tier = 7, super.name = "Hulk"});
+// // }
+
+// // class SuperMan extends Hero {
+// //   const SuperMan({required super.tier});
+// // }
