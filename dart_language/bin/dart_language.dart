935b98810c0a9db81cf90141ed360436d5f1baee
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2024-03-26 16:47:13 +0900

    update

--- dart_language/bin/dart_language.dart
+++ dart_language/bin/dart_language.dart
@@ -1,88 +1,43 @@
 //
 
-void main() {
-  IronMan ironMan = IronMan();
-}
+void main() {}
 
-abstract class Hero {
-  final String name;
-  const Hero(this.name);
-}
+abstract class Car {}
 
-mixin Fly on Hero {
-  void fly() => print("${super.name} Fly !");
-}
+mixin G80 on G70 {}
+mixin G70 on Car {}
 
-mixin Punch on Hero {
-  void punch() => print("${super.name} Punch !");
-}
-
-mixin Kick on Hero {
-  void kick() => print("${super.name} Kick !");
-}
-
-mixin Beam on Hero {
-  void beam() => print("${super.name} Laser Beam !");
-}
-
-class IronMan extends Hero with Fly, Punch, Kick, Beam {
-  IronMan() : super("IronMan");
-}
-
-class BatMan extends Hero with Punch, Kick {
-  const BatMan() : super("BatMan");
-}
-
-class SuperMan extends Hero with Fly, Punch, Kick, Beam {
-  SuperMan() : super("SuperMan");
-}
+class Test extends Car with G70, G80 {}
 
 // abstract class Hero {
-//   late final String name;
-//   void getAttack();
-//   void getDefence();
+//   final String name;
+//   const Hero(this.name);
 // }
 
-// mixin Mavel on Hero {
-//   final int tier = 9;
-//   @override
-//   void getAttack() => print("Mavel $name 공격력 : ${(10 - tier) * 100}");
-
-//   @override
-//   void getDefence() => print("Mavel $name 방어력 : ${(10 - tier) * 20}");
+// mixin Fly on Hero {
+//   void fly() => print("${super.name} Fly !");
 // }
 
-// mixin DC on Hero {
-//   int get attack;
-//   int get defence;
-//   @override
-//   void getAttack() => print("DC $name 공격력 : $attack");
-
-//   @override
-//   void getDefence() => print("DC $name 방어력 : $defence");
+// mixin Punch on Hero {
+//   void punch() => print("${super.name} Punch !");
 // }
 
-// mixin Actor {
-//   late final String actor;
-//   void getActor() => print("배우 : $actor");
+// mixin Kick on Hero {
+//   void kick() => print("${super.name} Kick !");
 // }
 
-// class IronMan extends Hero with Mavel, Actor {
-//   IronMan() {
-//     super.name = "IronMan";
-//     super.actor = "Robert Downey Jr.";
-//   }
+// mixin Beam on Hero {
+//   void beam() => print("${super.name} Laser Beam !");
 // }
 
-// class BatMan extends Hero with DC, Actor {
-//   BatMan() {
-//     super.name = "SuperMan";
-//     super.actor = "Christian Bale";
-//   }
+// class IronMan extends Hero with Fly, Punch, Kick, Beam {
+//   IronMan() : super("IronMan");
+// }
 
-//   @override
-//   int get attack => 1000;
+// class BatMan extends Hero with Punch, Kick {
+//   const BatMan() : super("BatMan");
+// }
 
-//   @override
-//   int get defence => 300;
+// class SuperMan extends Hero with Fly, Punch, Kick, Beam {
+//   SuperMan() : super("SuperMan");
 // }
