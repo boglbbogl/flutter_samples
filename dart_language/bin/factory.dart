4e49faf981a35156a29684b93697cea2c4d19d24
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2024-03-28 15:27:07 +0900

    update

--- dart_language/bin/factory.dart
+++ dart_language/bin/factory.dart
@@ -1,10 +1,1 @@
-class Macbook {
-  final int id;
-  final String name;
 
-  // const Macbook(this.id, this.name);
-
-  factory Macbook(int id, String name) => Macbook(id, name);
-}
-
-void main() {}
