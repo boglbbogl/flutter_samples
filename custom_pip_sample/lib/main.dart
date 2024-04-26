838cda3b43ea4c1968d435cef13c4a094f27ff4b
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2024-04-27 01:15:57 +0900

    delete pip_sample

--- custom_pip_sample/lib/main.dart
+++ custom_pip_sample/lib/main.dart
@@ -1,28 +1,12 @@
-import 'package:custom_pip_sample/application/mode_cubit.dart';
-import 'package:custom_pip_sample/presentation/main_page.dart';
-import 'package:custom_pip_sample/presentation/pip_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<ModeCubit>(
-      create: (_) => ModeCubit()..init(context),
-      child: const MaterialApp(
-        home: Stack(
-          children: [
-            MainPage(),
-            PIPWidget(),
-          ],
-        ),
-      ),
-    );
+    return const MaterialApp();
   }
 }
