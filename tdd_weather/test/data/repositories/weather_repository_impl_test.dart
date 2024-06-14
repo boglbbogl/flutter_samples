0be9dfc8b38303738a0341ca323d57791aaf6c7f
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2024-06-14 23:01:36 +0900

    upload

--- tdd_weather/test/data/repositories/weather_repository_impl_test.dart
+++ tdd_weather/test/data/repositories/weather_repository_impl_test.dart
@@ -1,26 +1,25 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-import 'package:tdd_weather/data/repositories/weather_repository_impl.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:tdd_weather/data/repositories/weather_repository_impl.dart';
 
-import '../../helpers/test_helper.mocks.dart';
+// import '../../helpers/test_helper.mocks.dart';
 
-void main() {
-  late MockWeatherRemoteDataSource mockWeatherRemoteDataSource;
-  late WeatherRepositoryImpl weatherRepositoryImpl;
+// void main() {
+//   // late MockWeatherRemoteDataSource mockWeatherRemoteDataSource;
+//   // late WeatherRepositoryImpl weatherRepositoryImpl;
 
-  setUp(() {
-    mockWeatherRemoteDataSource = MockWeatherRemoteDataSource();
-    weatherRepositoryImpl = WeatherRepositoryImpl(mockWeatherRemoteDataSource);
-  });
+//   setUp(() {
+//     mockWeatherRemoteDataSource = MockWeatherRemoteDataSource();
+//     // weatherRepositoryImpl = WeatherRepositoryImpl(mockWeatherRemoteDataSource);
+//   });
 
-  const String testCityName = "New York";
+//   // const String testCityName = "New York";
 
-  group("get current weather", () {
-    test(
-        "should return current weather when a call to data source is successful",
-        () async {
-      // when(mockWeatherRemoteDataSource.getCurrentWeather(testCityName))
-      //     .thenAnswer((_) async => testWeatherModel);
-    });
-  });
-}
+//   group("get current weather", () {
+//     test(
+//         "should return current weather when a call to data source is successful",
+//         () async {
+//       // when(mockWeatherRemoteDataSource.getCurrentWeather(testCityName))
+//       //     .thenAnswer((_) async => testWeatherModel);
+//     });
+//   });
+// }
