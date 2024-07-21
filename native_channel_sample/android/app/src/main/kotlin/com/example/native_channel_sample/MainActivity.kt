5892f7384044bcb12496edb47ffd6cc4de286780
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2024-07-21 20:59:43 +0900

    update

--- native_channel_sample/android/app/src/main/kotlin/com/example/native_channel_sample/MainActivity.kt
+++ native_channel_sample/android/app/src/main/kotlin/com/example/native_channel_sample/MainActivity.kt
@@ -1,5 +1,18 @@
 package com.example.native_channel_sample
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import androidx.annotation.NonNull
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "native/sample").setMethodCallHandler {
+                call, result ->
+            if (call.method == "get") {
+                result.success("test")
+            }
+        }
+    }
+}
--- native_channel_sample/ios/Runner/AppDelegate.swift
+++ native_channel_sample/ios/Runner/AppDelegate.swift
@@ -7,6 +7,20 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      
+      let controller = window?.rootViewController as! FlutterViewController
+         let methodChannel = FlutterMethodChannel(name: "native/sample", binaryMessenger: controller.binaryMessenger)
+         
+         methodChannel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
+             guard call.method == "get" else {
+                 result(FlutterMethodNotImplemented)
+                 return
+             }
+             let count = NSUserDefaults.standard.integer(forKey: "count")
+             result(count)
+         }
+      
+      
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
